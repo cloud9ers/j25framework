3a30f8d9643aaedc8a420318346051889a4fd7b8
Ahmed Soliman <asoliman@cloud9ers.com>
2011-06-07 13:33:26 +0200

    removed coverage

--- test/xmlRunner.sh
+++ test/xmlRunner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=`dirname $0`/..:$PYTHONPATH
-python textrunner.py -l INFO -m ".*[Tt]est" --with-cov --cov ../ --cov-report xml --with-xunit --xunit-file report.xml $*
+python textrunner.py -l INFO -m ".*[Tt]est" --with-xunit --xunit-file report.xml $*
