281c4137f994db0308b2f339c9994bc9f3d927f3
Ahmed Soliman <asoliman@cloud9ers.com>
2012-05-23 22:21:06 +0200

    fixed JSONCustomeEncoder to fit pymongo 2.2

--- j25/utils/JSONCustomEncoder.py
+++ j25/utils/JSONCustomEncoder.py
@@ -3,9 +3,6 @@ from datetime import datetime, date, time
 from bson.objectid import ObjectId
 from mongoengine.document import Document
 from mongoengine.queryset import QuerySet
-from pymongo import dbref
-from mongoengine import base
-import j25
 
 class JSONCustomEncoder(JSONEncoder):
     def default(self, obj):
@@ -18,9 +15,6 @@ class JSONCustomEncoder(JSONEncoder):
                 doc.pop('_types')
             except:
                 pass
-            return doc
-        if isinstance(obj, dbref.DBRef):
-            doc = j25._store[obj.collection].find_one({'_id': ObjectId(obj.id)})
             return doc
         if isinstance(obj, ObjectId):
             return str(obj)
